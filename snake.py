179328f77e958e5c60d266675bf410e91c54f775
Andres Felipe Aparicio Mestre <anapariciom@unal.edu.co>
2022-09-20 18:09:29 -0500

    Game finished

--- snake.py
+++ snake.py
@@ -4,102 +4,141 @@ from collections import deque as dq
 import random as rn
 import pygame as pg
 
-pg.init()
-score = 0
-running = True
-screen = pg.display.set_mode((800,800))
-direction = "up"
-board = [[0 for j in range(13)] for i in range(13)]
-snake = dq([(6,6), (7,6), (8,6)])
-for i in snake:
-  board[i[0]][i[1]]=2
 
-def updateBoard(coord,value):
-    if(coord[0]>12 or coord[0]<0 or coord[1]>12 or coord[1]<0):
+
+class Snake:
+    def __init__(self):
+        self.body = dq([(6,6), (6,7), (6,8)])
+        self.direction = "up"
+        self.grow = False
+        self.score = 0
+    
+    def changeDir(self, newDir):
+        if self.direction=="up":
+            if(newDir=="left" or newDir=="right" or newDir=="up"):
+                self.direction=newDir
+        elif self.direction=="right":
+            if(newDir=="down" or newDir=="right" or newDir=="up"):
+                self.direction=newDir
+        elif self.direction=="down":
+            if(newDir=="left" or newDir=="right" or newDir=="down"):
+                self.direction=newDir
+        elif self.direction=="left":
+            if(newDir=="left" or newDir=="down" or newDir=="up"):
+                self.direction=newDir
+
+    def eatApple(self,apple):
+        if(self.body[0] == apple.coord):
+            apple.generate()
+            self.grow = True
+            return True
         return False
-    board[coord[0]][coord[1]]=value
-    return True
 
-def fruitGeneration():
-  i = rn.randint(0,12)
-  j = rn.randint(0,12)
-  if board[i][j]!=0:
-    return fruitGeneration()
-  coord = (i,j)
-  board[i][j]=1
-  return coord
+    def draw(self):
+        for block in self.body:
+            pg.draw.rect(WIN,(0,255,0),(block[0]*GRIDSIZE,block[1]*GRIDSIZE,GRIDSIZE,GRIDSIZE))
+           
 
-def changeDir(currentDir, newDir):
-    if currentDir=="up":
-        if(newDir=="left" or newDir=="right" or newDir=="up"):
-            return(newDir)
-    elif currentDir=="right":
-        if(newDir=="down" or newDir=="right" or newDir=="up"):
-            return(newDir)
-    elif currentDir=="down":
-        if(newDir=="left" or newDir=="right" or newDir=="down"):
-            return(newDir)
-    elif currentDir=="left":
-        if(newDir=="left" or newDir=="down" or newDir=="up"):
-            return(newDir)
-    return(currentDir)
+    def moveSnake(self):
+        if(self.direction=="up"):
+            newCoord = (self.body[0][0], self.body[0][1]-1)
+           
+        elif(self.direction=="down"):
+            newCoord = (self.body[0][0], self.body[0][1]+1)
+            
+        elif(self.direction=="right"):
+            newCoord = (self.body[0][0]+1,  self.body[0][1])
+           
+        elif(self.direction=="left"):
+            newCoord = (self.body[0][0]-1,  self.body[0][1])
 
-def eatFruit(coord):
-    score+=1
-    board[coord[0]][coord[1]] = 2
-    return fruitGeneration(board)
+        self.body.appendleft(newCoord)
+        if (not self.grow):
+            self.body.pop()
+        else:
+            self.grow = False
+            
+    def reset(self, apple):
+        self.body = dq([(6,6), (6,7), (6,8)])
+        self.direction = "up"
+        self.grow = False
+        self.score = 0
+        apple.generate()
 
-def moveSnake():
-    if(direction=="up"):
-        newCoord = (snake[0][0]-1, snake[0][1])
-        deleted = snake.pop()
-        if(updateBoard(newCoord,2)):
-            updateBoard(deleted,0)        
-            snake.appendleft(newCoord)
-        else:
-            return False
-    elif(direction=="down"):
-        newCoord = (snake[0][0]+1, snake[0][1])
-        deleted = snake.pop()
-        if(updateBoard(newCoord,2)):
-            updateBoard(deleted,0)        
-            snake.appendleft(newCoord)
-        else:
-            return False
-    elif(direction=="right"):
-        newCoord = (snake[0][0],  snake[0][1]+1)
-        deleted = snake.pop()
-        if(updateBoard(newCoord,2)):
-            updateBoard(deleted,0)        
-            snake.appendleft(newCoord)
-        else:
-            return False
-    elif(direction=="left"):
-        newCoord = (snake[0][0],  snake[0][1]-1)
-        deleted = snake.pop()
-        if(updateBoard(newCoord,2)):
-            updateBoard(deleted,0)        
-            snake.appendleft(newCoord)
-        else:
-            return False
-    return True
-       
-fruit = fruitGeneration()
-print(board)
-while running:
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT or event.key == pg.K_a:
-                direction = changeDir(direction, "left")
-            elif event.key == pg.K_UP or event.key == pg.K_w:
-                direction = changeDir(direction, "up")
-            elif event.key == pg.K_RIGHT or event.key == pg.K_d:
-                direction = changeDir(direction, "right")
-            elif event.key == pg.K_DOWN or event.key == pg.K_s:
-                direction = changeDir(direction, "down")
-            if(not moveSnake()):
-                running=False
-            print(board)    
-        if event.type == pg.QUIT:
-            running = False
-pg.quit()
+    def die(self):
+        if self.body[0][0] > 12 or self.body[0][1] > 12 or self.body[0][0] <0  or self.body[0][1] < 0:
+            return True
+
+        for i in range(1, len(self.body)):
+            if self.body[0] == self.body[i]:
+                return True
+
+class Apple:
+    def __init__(self, snake):
+        self.snake = snake
+        self.generate()
+        
+    
+    def draw(self):
+        pg.draw.rect(WIN,(255,0,0),(self.coord[0]*GRIDSIZE,self.coord[1]*GRIDSIZE,GRIDSIZE,GRIDSIZE))
+
+    def generate(self):
+        self.x = rn.randint(0,12)
+        self.y = rn.randint(0,12)
+        self.coord = (self.x,self.y)
+        if self.coord in self.snake.body:
+                self.generate()
+
+pg.init()      
+HEIGHT = 480
+WIDTH = 480
+GRID_WIDTH = 13
+GRID_HEIGHT = 13
+GRIDSIZE = HEIGHT/GRID_WIDTH
+WIN = pg.display.set_mode((WIDTH,HEIGHT))
+SCORE_TEXT= pg.font.SysFont("Russo One", 25)
+
+def main():
+    
+    snake = Snake()
+    apple = Apple(snake)
+    WIN = pg.display.set_mode((WIDTH,HEIGHT), 0, 32)
+    running = True
+    clock = pg.time.Clock()  
+
+    while running:
+        clock.tick(10)
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_LEFT or event.key == pg.K_a:
+                    snake.changeDir("left")
+                elif event.key == pg.K_UP or event.key == pg.K_w:
+                    snake.changeDir("up")
+                elif event.key == pg.K_RIGHT or event.key == pg.K_d:
+                    snake.changeDir("right")
+                elif event.key == pg.K_DOWN or event.key == pg.K_s:
+                    snake.changeDir("down")
+            if event.type == pg.QUIT:
+                quit()
+    
+        WIN.fill((0,0,0))
+        snake.draw()
+        apple.draw()
+        snake.moveSnake()
+
+        if(snake.die()):
+            snake.reset(apple)
+
+        if(snake.eatApple(apple)):
+            snake.score+=1
+        
+        text = SCORE_TEXT.render("Score: {0}".format(snake.score),1,(255,255,255))
+        WIN.blit(text,(WIDTH-text.get_width()-10,10))
+        pg.display.update()
+        
+main()
+
+
+
+
+
